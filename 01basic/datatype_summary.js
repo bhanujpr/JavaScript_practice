5fe1a39010416e045afbc35fa5b1b1671335aeca
Bhanu pratap <bhanurajawatt@gmail.com>
2024-01-07 20:36:59 +0530

    learning and practicing basic js variables and datatypes

--- 01basic/datatype_summary.js
+++ 01basic/datatype_summary.js
@@ -11,7 +11,7 @@ const outsideTemp=null
 let userEmail;
 const id=Symbol('123')
 const anotherId=Symbol('123')
-console.log(id===anotherId)
+// console.log(id===anotherId)
 
 
 const bigNumber=33333333333333333n
@@ -27,7 +27,33 @@ let obj={
 }
 
 const myFun = function(){
-    console.log("Hello world")
+    // console.log("Hello world")
 }
 myFun()
-console.log(typeof myFun())
+// console.log(typeof myFun())
+
+
+
+
+///+++++++++++++++++++++++++++++++++++++++++++
+
+//Stack(primitive),Heap(non primitive)
+
+
+let myName="BhanupratapSingh"
+let anotherName=myName
+anotherName="Helloababa"
+console.log(myName)
+console.log(anotherName)
+
+let userOne={
+    email:"Helllo@gmail.com",
+    upi:"userupi@ybl",
+}
+let userTwo=userOne
+userTwo.email="Bhanu@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
+//In heap both get on reference where in stack another copy generated
