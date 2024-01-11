c83a8929f5358f844058207c9a1e5d21efe9a34a
Bhanu pratap <bhanurajawatt@gmail.com>
2024-01-11 21:18:56 +0530

    more project

---
+++ 07projects/bg-changer/script.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll('.button')
+console.log(buttons)
+
+
+const body = document.querySelector('body')
+
+
+buttons.forEach(function(button){
+    button.addEventListener('click',function(e){
+        body.style.backgroundColor=e.target.id;
+    })
+})
